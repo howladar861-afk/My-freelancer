507379311e268aa3b705f32ad25c108ce34f4035
howladar861-afk <howladar861@gmail.com>
2026-08-07 10:43:07 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+// Firebase SDK Import
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
 
+// আপনার Firebase Config
+const firebaseConfig = {
+  apiKey: "AIzaSyBFUKPT7fo6sUofdO09ffiZgjdlaR5evm8",
+  authDomain: "rakib-freelancer-9c66b.firebaseapp.com",
+  projectId: "rakib-freelancer-9c66b",
+  storageBucket: "rakib-freelancer-9c66b.firebasestorage.app",
+  messagingSenderId: "541209844482",
+  appId: "1:541209844482:web:510568d5226c9bf47ac01b",
+  measurementId: "G-F6QNE0QN5K"
+};
+
+// Firebase চালু
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Register
+document.getElementById("registerBtn").onclick = () => {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Registration Successful");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
+
+// Login
+document.getElementById("loginBtn").onclick = () => {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      alert("Login Successful");
+    })
+    .catch((error) => {
+      alert(error.message);
+    });
+};
