24da888f974a41b513ae400eea519e2aed072793
howladar861-afk <howladar861@gmail.com>
2026-08-12 00:23:41 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -81,11 +81,11 @@ function escapeHtml(value) {
   }
 
   return String(value)
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#039;");
+  .replace(/&/g, "&amp;")
+  .replace(/</g, "&lt;")
+  .replace(/>/g, "&gt;")
+  .replace(/"/g, "&quot;")
+  .replace(/'/g, "&#039;");
   }
 }
 
