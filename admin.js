c32d92a632430987e8096ee60870be0a35d19ccf
howladar861-afk <howladar861@gmail.com>
2026-08-25 13:19:42 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1122,14 +1122,40 @@ const result =
     db,
     id
   );
-if (
-  !result.alreadyPaid &&
-  result.level1Uid
-) {
-  await addDailyReferral(
-    db,
-    result.level1Uid
-  );
+// =====================================
+// DAILY REFERRAL COUNT +1
+// =====================================
+
+const userRef = doc(db, "users", userId);
+
+const userSnap = await getDoc(userRef);
+
+if (userSnap.exists()) {
+
+  const userData = userSnap.data();
+
+  const referrerUid = userData.referredBy;
+
+  if (referrerUid) {
+
+    const counted = await addDailyReferral(
+      db,
+      referrerUid
+    );
+
+    console.log(
+      "Daily Referral Count:",
+      counted
+    );
+
+  } else {
+
+    console.log(
+      "এই User-এর referredBy নেই"
+    );
+
+  }
+}
 }
 await setDoc(
   doc(db, "users", userId),
